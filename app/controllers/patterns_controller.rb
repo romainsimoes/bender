69052385168d7115b51d66f778b06574a113060f
romainsimoes <romain.simoes.pro@gmail.com>
2016-08-22 14:52:38 +0200

    Pattern Controller

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -1,73 +1,49 @@
 class PatternsController < ApplicationController
   before_action :set_pattern, only: [:show, :edit, :update, :destroy]
+  before_action :find_bot, only: [:new, :edit, :create, :update, :destroy]
 
-  # GET /patterns
-  # GET /patterns.json
   def index
     @patterns = Pattern.all
   end
 
-  # GET /patterns/1
-  # GET /patterns/1.json
-  def show
-  end
-
-  # GET /patterns/new
   def new
     @pattern = Pattern.new
   end
 
-  # GET /patterns/1/edit
   def edit
   end
 
-  # POST /patterns
-  # POST /patterns.json
   def create
     @pattern = Pattern.new(pattern_params)
-
-    respond_to do |format|
-      if @pattern.save
-        format.html { redirect_to @pattern, notice: 'Pattern was successfully created.' }
-        format.json { render :show, status: :created, location: @pattern }
-      else
-        format.html { render :new }
-        format.json { render json: @pattern.errors, status: :unprocessable_entity }
-      end
+    if @pattern.save
+      redirect_to bot_path(@bot), notice: 'Pattern was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /patterns/1
-  # PATCH/PUT /patterns/1.json
   def update
-    respond_to do |format|
-      if @pattern.update(pattern_params)
-        format.html { redirect_to @pattern, notice: 'Pattern was successfully updated.' }
-        format.json { render :show, status: :ok, location: @pattern }
-      else
-        format.html { render :edit }
-        format.json { render json: @pattern.errors, status: :unprocessable_entity }
-      end
+    if @pattern.update(pattern_params)
+      redirect_to bot_path(@bot), notice: 'Pattern was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /patterns/1
-  # DELETE /patterns/1.json
   def destroy
     @pattern.destroy
-    respond_to do |format|
-      format.html { redirect_to patterns_url, notice: 'Pattern was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to bot_path(@bot), notice: 'Pattern was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_pattern
       @pattern = Pattern.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def find_bot
+      @bot = Bot.find(params[:bot_id])
+    end
+
     def pattern_params
       params.require(:pattern).permit(:trigger, :answer)
     end
