2dee7e056a46c5a5a8d0c0c9bf19e7c5bb409637
francois hubert <francois725@hotmail.fr>
2016-08-24 14:59:12 +0200

    m

---
+++ app/jobs/process_bot_message_job.rb
@@ -0,0 +1,20 @@
+class ProcessBotMessageJob < ApplicationJob
+  queue_as :default
+
+  def perform(message_sender_id, message_text, bot)
+
+    # 1 utiliser bot + message pour essayer de trouver la réponse
+    hash = bot.match_pattern(message_text)
+    message = hash[:answer]
+    pattern = hash[:pattern]
+
+    unless message
+      message = "default message"
+    end
+
+    # 2 si il y a une réponse envoyer la réponse
+    FacebookRequestService.send_message(message_sender_id, message, bot.page_access_token)
+    # 3 enregistrer dans history
+    History.create(question: message_text, answer: message, bot_id: bot, pattern_id: pattern )
+  end
+end
