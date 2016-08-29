104d2dbb5d53bb1b6983f48b67aa66497f28d08a
romainsimoes <romain.simoes.pro@gmail.com>
2016-08-29 17:44:25 +0200

    edit fixed

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -103,8 +103,10 @@ class BotsController < ApplicationController
 
     def get_opening_times
       @opening_and_closing = ''
-      @bot.info['result']['opening_hours']['weekday_text'].each do |day|
-        @opening_and_closing += "#{day}\n"
+      if @bot.info
+        @bot.info['result']['opening_hours']['weekday_text'].each do |day|
+          @opening_and_closing += "#{day}\n"
+        end
       end
     end
 
