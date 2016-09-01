d1b1aa9a89280a6ebbc68cf52afc2a36943ddfc4
romainsimoes <romain.simoes.pro@gmail.com>
2016-09-01 14:29:27 +0200

    agenda entry

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160831101225) do
-=======
 ActiveRecord::Schema.define(version: 20160831134721) do
->>>>>>> ea0ff4d18e5cbf661201dc57b0e8abb4d69062d6
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +94,7 @@ ActiveRecord::Schema.define(version: 20160831134721) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.boolean  "admin",                  default: false
     t.string   "provider"
     t.string   "uid"
     t.string   "facebook_picture_url"
@@ -105,7 +102,6 @@ ActiveRecord::Schema.define(version: 20160831134721) do
     t.string   "last_name"
     t.string   "token"
     t.datetime "token_expiry"
-    t.boolean  "admin",                  default: false
     t.string   "google_uid"
     t.string   "google_token"
     t.string   "google_email"
